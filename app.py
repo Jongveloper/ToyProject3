5554c89edc92c2ec026d4f39b038f21f1b474922
JongHyukWoo <kjop7419@naver.com>
2021-05-03 12:35:10 +0900

    회원가입 구현!!

--- app.py
+++ app.py
@@ -18,35 +18,31 @@ client = MongoClient('localhost', 27017)
 db = client.dbname
 
 @app.route('/')
-def sign_up():
-    return render_template('sign_up.html')
+def home():
+    return render_template('login.html')
 
 #회원가입
-@app.route('/sign_info', methods=['GET','POST'])
+@app.route('/register', methods=['GET','POST'])
 def register():
     if request.method == 'GET':
         return render_template("sign_up.html")
     else:
-        username = request.form.get('username_give')
-        email = request.form.get('email_give')
-        ordinal = request.form.get('ordinal_give')
-        userid = request.form.get('userid_give')
-        password = request.form.get('password_give')
-        re_password = request.form.get('re_password_give')
+        username = request.form.get('username')
+        email = request.form.get('email')
+        userid = request.form.get('userid')
+        password = request.form.get('password')
+        re_password = request.form.get('re_password')
 
-        userinfo = {'user_id': userid, 'user_name': username, 'user_pw': password,
-                    'user_email': email, 'user_ordinal': ordinal}
+        doc = {'user_id': userid, 'user_name': username, 'user_pw': password, 'user_mail': email}
 
-        if not (userid and username and password and re_password):
+        if not (userid and username and password and re_password and email):
             return "모두 입력해주세요!"
         elif password != re_password:
             return "비밀번호가 일치하지 않습니다."
         else:
-            db.users.insert_one(userinfo)
+            db.users.insert_one(doc)
             return "회원가입이 완료되었습니다!"
-        return redirect('/sign_up')
-
-    db.name.insert_one(doc)
+        return redirect(url_for('login'))
 
 if __name__ == '__main__':
    app.run('0.0.0.0',port=5000,debug=True)
